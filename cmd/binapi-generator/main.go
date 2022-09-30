41889009cfb493916c81805947df472cab037e37
Ed Warnicke <hagbard@gmail.com>
2022-09-30 12:04:26 -0500

    Added a doc.go to binapi/ to ensure it always has a package.
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- cmd/binapi-generator/main.go
+++ cmd/binapi-generator/main.go
@@ -107,6 +107,7 @@ func main() {
 			if !file.Generate {
 				continue
 			}
+			binapigen.GenerateDoc(gen)
 			binapigen.GenerateAPI(gen, file)
 			for _, p := range genPlugins {
 				if err := binapigen.RunPlugin(p, gen, file); err != nil {
