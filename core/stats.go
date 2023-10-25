7db1bdbacb991afbcf6a07c0ca2b6bf3261c19c3
Shinae Woo <shinae2012@gmail.com>
2023-10-25 15:34:45 -0700

    Public Wrapper for '(c *StatsConnection) UpdateStats'

--- core/stats.go
+++ core/stats.go
@@ -134,6 +134,11 @@ func (c *StatsConnection) disconnectClient() {
 	}
 }
 
+// Public wrapper for updating stats from patters
+func (c *StatsConnection) UpdateStats(statDir **adapter.StatDir, patterns ...string) error {
+	return c.updateStats(statDir, patterns...)
+}
+
 func (c *StatsConnection) updateStats(statDir **adapter.StatDir, patterns ...string) error {
 	if statDir == nil {
 		panic("statDir must not nil")
